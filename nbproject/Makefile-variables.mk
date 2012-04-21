5e542d5b4578830a9314de940d7151109fe05732
Walace Rocha <walacesrocha@yahoo.com.br>
2012-04-21 11:15:09 -0300

    a

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Linux-x86
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=timetabling
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Linux-x86/timetabling
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/timetabling
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=timetabling.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Linux-x86/package/timetabling.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/timetabling.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=
+CND_ARTIFACT_DIR_Release=dist/Release/
 CND_ARTIFACT_NAME_Release=timetabling
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/timetabling
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release//timetabling
+CND_PACKAGE_DIR_Release=dist/Release//package
 CND_PACKAGE_NAME_Release=timetabling.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/timetabling.tar
+CND_PACKAGE_PATH_Release=dist/Release//package/timetabling.tar
 #
 # include compiler specific variables
 #
